a95acd2161734bbfbaf47f6cf6d788fa015ea1a9
J378843 <J378843@P-B223-176147.student.central>
2018-05-01 16:06:26 +0800

    Initial

---
+++ Group123/src/group123/Group123.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group123;
+
+/**
+ *
+ * @author J378843
+ */
+public class Group123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
