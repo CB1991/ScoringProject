9fed469f0a6117429292f2cb238845fef2d4a456
J378843 <J378843@P-B223-176147.student.central>
2018-05-02 13:57:05 +0800

    Testing with objects and show changes when plays score

--- Group123/src/group123/Group123.java
+++ Group123/src/group123/Group123.java
@@ -5,17 +5,99 @@
  */
 package group123;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author J378843
  */
 public class Group123 {
 
+   
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+         
+      //  int id, String userName, String teamName, int totalPlaying, int totalScored
+     
+                
+        ArrayList<Player> westEagle = new ArrayList<Player>();
+        
+           Player w1 = new Player(1,"PlayerName","West Eagle",1,0);
+           Player w2 = new Player(1,"PlayerName","West Eagle",1,0); 
+           Player w3 = new Player(1,"PlayerName","West Eagle",1,0);
+           Player w4 = new Player(1,"PlayerName","West Eagle",1,0); 
+           Player w5 = new Player(1,"PlayerName","West Eagle",1,0);
+     
+                                       
+           westEagle.add(w1);
+           westEagle.add(w2);
+           westEagle.add(w3);
+           westEagle.add(w4);
+           westEagle.add(w5);
+        
+           //(String whoGoal, int whichRound, int score)
+        
+           // player w1 goals in Round 1 and 1 Score
+           Goal g1 = new Goal(w1, 1,1);
+           
+           // Change w1's Total Score
+            w1.setTotalScored(1);
+            
+            
+            // Test display changed totalscore's change
+             System.out.println("totalscore"+w1.getTotalScored());
+             System.out.println("totalscore_"+ westEagle.get(0).getTotalScored());
+            
+           // player w1 goals in Round 1 and 5 Score
+           Goal g2 = new Goal(w2, 1,5);                      
+           
+           // Change w2's Total Score
+           w2.setTotalScored(5);
+            
+           // Test display changed totalscore's change
+            System.out.println("totalscore"+w2.getTotalScored());
+             System.out.println("totalscore_"+ westEagle.get(1).getTotalScored());
+
+           
+           // Displaying user totalscored
+             for(int i=0; i<westEagle.size(); i++)
+                 {
+                    System.out.println(westEagle.get(i).getTotalScored());
+                 }
+                                    
         // TODO code application logic here
+                                                 
     }
+    
+    public void Valiate() // Method valiate user's input is numeric
+    {
+    Scanner input = new Scanner(System.in);
+
+    int userType = 0;
+
+    boolean isUserInputNumeric = true;
+
+    boolean isNumThere;
+
+    while(isUserInputNumeric==true)
+        {
+            if(input.hasNextInt())
+            {
+                userType= input.nextInt();
+                isUserInputNumeric = false;        
+            }
+            else if (!input.hasNextInt())
+            {
+            System.out.println("Please type numeric numbers");
+            input.next();
+            }
+        }           
+    }
+    
     
 }
